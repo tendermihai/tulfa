366675434353bdbb622b9697ec8e580ffcbc81d7
Tender <tender121995@gmail.com>
2024-11-02 20:00:19 +0200

    task-2-create-main-done-added-footer-done

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Main from "../components/Main";
+import Footer from "../components/Footer";
 
 function HomePage() {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
